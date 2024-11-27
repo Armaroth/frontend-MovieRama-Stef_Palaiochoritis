9baa7cfbdfc8c2450f409c5ddc01895848202e80
Stefanos <spalaio@moby-it.com>
2024-11-27 21:05:43 +0200

    Ui
    Created styles folder to store the .css files to give the app
     the desired look
    Enhanced the html returned by the fetchMovies function

--- dist/js/api/tmdb-api.js
+++ dist/js/api/tmdb-api.js
@@ -13,26 +13,59 @@ export const genres = await (async function fetchGenres() {
         console.error('Error fetching genres:', error);
     }
 })();
-console.log('Genres fetched:', genres);
+// console.log('Genres fetched:', genres);
+// const renderLoadingScreen = () => {
+//     document.innerHtml = 'Loading';
+// }
 export async function fetchMovies() {
     try {
+        // renderLoadingScreen();
         const response = await fetch(`${TMDB_BASE_URL}/movie/now_playing?api_key=${TMDB_API_KEY}&language=en-US&page=1`);
         const data = await response.json();
+        // isLoading = false;
         const { success, output: movies } = v.safeParse(MoviesSchema, data.results);
         const contentDiv = document.getElementById('content');
         if (success) {
-            console.log('now playing', movies.map(movie => movie.genres));
+            console.log('now playing', movies.map(movie => movie));
             const movieList = movies.map(movie => {
                 return `
-          <div class="movie-card">
-            <h3>${movie.title}</h3>
-            <p>${movie.overview}</p>
-            <img src="https://image.tmdb.org/t/p/w500/${movie.posterPath}" alt="${movie.title}" />
-          </div>
+         <section class="movie-card">
+          <section class="image-container" >
+          <img src="https://image.tmdb.org/t/p/w500/${movie.posterPath}" alt="${movie.title}" />
+          </section>
+          <section class="movie-data">
+          <h3>${movie.title}</h3>
+          <p class="overview">${movie.overview}</p>
+          <section class='movie-date'> 
+          <span class='attribute'>
+            Year:
+          </span>
+           <span class='value'>
+           ${movie.releaseDate.split('-')[0]}</section>
+          </span>
+          <section>
+          <span class='attribute'>
+          Genres: 
+         </span>
+          <span class='value'>
+          ${movie.genres.map(genre => ` ${genre?.name}`)}
+         </span> 
+         </section>
+         <section>
+          <span class='attribute'>
+          Vote average:
+         </span>
+          <span class='value'>
+          ${movie.voteAverage.toFixed(1)}
+         </span>
+          </section>  
+          </section>
+          </section>
         `;
             }).join('');
             if (contentDiv)
-                contentDiv.innerHTML = movieList;
+                contentDiv.innerHTML = contentDiv.innerHTML + movieList;
+            // loading screen 
         }
     }
     catch (error) {
--- src/js/api/tmdb-api.ts
+++ src/js/api/tmdb-api.ts
@@ -14,26 +14,61 @@ export const genres: { id: number, name: string }[] = await (async function fetc
     }
 })();
 
-console.log('Genres fetched:', genres);
+// console.log('Genres fetched:', genres);
+
+// const renderLoadingScreen = () => {
+//     document.innerHtml = 'Loading';
+// }
 
 export async function fetchMovies() {
     try {
+        // renderLoadingScreen();
         const response = await fetch(`${TMDB_BASE_URL}/movie/now_playing?api_key=${TMDB_API_KEY}&language=en-US&page=1`);
         const data = await response.json();
+
+        // isLoading = false;
         const { success, output: movies } = v.safeParse(MoviesSchema, data.results);
         const contentDiv = document.getElementById('content');
         if (success) {
-            console.log('now playing', movies.map(movie => movie.genres))
+            console.log('now playing', movies.map(movie => movie))
             const movieList = movies.map(movie => {
                 return `
-          <div class="movie-card">
-            <h3>${movie.title}</h3>
-            <p>${movie.overview}</p>
-            <img src="https://image.tmdb.org/t/p/w500/${movie.posterPath}" alt="${movie.title}" />
-          </div>
+         <section class="movie-card">
+          <section class="image-container" >
+          <img src="https://image.tmdb.org/t/p/w500/${movie.posterPath}" alt="${movie.title}" />
+          </section>
+          <section class="movie-data">
+          <h3>${movie.title}</h3>
+          <p class="overview">${movie.overview}</p>
+          <section class='movie-date'> 
+          <span class='attribute'>
+            Year:
+          </span>
+           <span class='value'>
+           ${movie.releaseDate.split('-')[0]}</section>
+          </span>
+          <section>
+          <span class='attribute'>
+          Genres: 
+         </span>
+          <span class='value'>
+          ${movie.genres.map(genre => ` ${genre?.name}`)}
+         </span> 
+         </section>
+         <section>
+          <span class='attribute'>
+          Vote average:
+         </span>
+          <span class='value'>
+          ${movie.voteAverage.toFixed(1)}
+         </span>
+          </section>  
+          </section>
+          </section>
         `;
             }).join('');
-            if (contentDiv) contentDiv.innerHTML = movieList;
+            if (contentDiv) contentDiv.innerHTML = contentDiv.innerHTML + movieList;
+            // loading screen 
         }
     } catch (error) {
         console.error('Error fetching data from TMDB:', error);

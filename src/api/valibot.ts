57b15f34ad6f2f70b2849d6334adaef2f2455406
Stefanos <spalaio@moby-it.com>
2024-11-28 18:13:20 +0200

    Valibot config
    Changed the valibot schema to take account different movie objects fetched by the api

--- src/api/valibot.ts
+++ src/api/valibot.ts
@@ -4,17 +4,16 @@ export const MovieSchema = v.pipe(
     v.object({
         title: v.string(),
         original_title: v.string(),
-        backdrop_path: v.nullable(v.string()),
+        backdrop_path: v.optional(v.nullable(v.string())),
         genre_ids: v.array(v.number()),
         id: v.number(),
         overview: v.string(),
-        poster_path: v.string(),
+        poster_path: v.optional(v.nullable(v.string())),
         vote_average: v.number(),
         release_date: v.string()
     }),
     v.transform((o) => ({
         title: o.title,
-        backdropPath: o.backdrop_path,
         genres: o.genre_ids.map((genreId: number) => {
             const genre = genres.find(g => g.id === genreId);
             return genre;
@@ -22,7 +21,7 @@ export const MovieSchema = v.pipe(
         id: o.id,
         originalTitle: o.original_title,
         overview: o.overview,
-        posterPath: o.poster_path,
+        posterPath: o.poster_path ? o.poster_path : o.backdrop_path,
         voteAverage: o.vote_average,
         releaseDate: o.release_date
     }))

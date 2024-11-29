1ef1e186b8a8b758d4b69adbd426a8b6770b8862
Stefanos <spalaio@moby-it.com>
2024-11-29 20:38:14 +0200

    Scroll event
    Moved the scroll functionality from the scroll.ts file to the events. ts file

--- src/events.ts
+++ src/events.ts
@@ -1,27 +1,42 @@
-import { TMDB_API_KEY, TMDB_BASE_URL } from "./constants";
-import { renderInTheatersPage } from "./utils/inTheaters";
-import { resetPageCounter } from "./utils/scroll";
-import { resetHtml } from "./utils/utils";
-
+import { urlRoutes } from "./route";
+import { getCurrentPage, getCurrentRoute, getIsFetching, setCurrentRoute, setIsFetching } from "./state";
+import { navigateTo } from "./urlRouter";
+import { loadMoviesPage, resetHtml } from "./utils/utils";
 const searchInput = document.getElementById('search') as HTMLInputElement;
 let debounceTimeout: ReturnType<typeof setTimeout>;
-let state: 'starter' | 'searching' = 'starter';
 searchInput.addEventListener('input', (event: Event) => {
     clearTimeout(debounceTimeout);
     debounceTimeout = setTimeout(async () => {
         resetHtml();
-        const page = resetPageCounter();
-        let endpoint = '';
-        if (!searchInput.value && state !== 'starter') {
-            endpoint = `${TMDB_BASE_URL}/movie/now_playing?api_key=${TMDB_API_KEY}&language=en-US&page=${page}`;
-            state = 'starter';
-        } else {
-            const query = searchInput.value.trim();
-            if (query) {
-                endpoint = `${TMDB_BASE_URL}/search/movie?api_key=${TMDB_API_KEY}&query=${encodeURIComponent(query)}&page=${page}`;
-                state = 'searching';
-            }
+        scrollTo(0, 0);
+        if (searchInput.value) {
+            setCurrentRoute('/search');
+            navigateTo('/search');
         }
-        renderInTheatersPage(endpoint)
+        else {
+            setCurrentRoute('/');
+            navigateTo('/');
+        }
     }, 300);
 });
+window.addEventListener("scroll", handleScroll);
+export async function handleScroll() {
+    let scrollTimeout: NodeJS.Timeout | null = null;
+    if (scrollTimeout) return;
+    scrollTimeout = setTimeout(async () => {
+        const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+        if (scrollTop + clientHeight >= scrollHeight - 10 && !getIsFetching()) {
+            const page = getCurrentPage();
+            const route = getCurrentRoute();
+            setIsFetching(true);
+            if (urlRoutes[route].fetch) {
+                const movies = await urlRoutes[route].fetch(page);
+                if (!movies.length) {
+                    return;
+                }
+                await loadMoviesPage(movies)
+            }
+            setIsFetching(false);
+        }
+    }, 400);
+}
